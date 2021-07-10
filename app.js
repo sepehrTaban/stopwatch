0d10f82beb8a46c308b6e097862aa078d4d7ecf0
Sepehr Taban <sepehr.taban77@gmail.com>
2021-07-11 00:15:33 +0430

    add pause and resume function for the main watch

--- app.js
+++ app.js
@@ -25,48 +25,78 @@ let lapCount = 0;
 
 
 /*############################################*/
-startBtn.addEventListener('click', () => {
-    start();    
+startBtn.addEventListener('click', ()=> {
+    start();
 })
-startBtn.addEventListener('touchstart', () => {
-    start();    
+startBtn.addEventListener('touchstart', ()=> {
+    start();
+})
+/**************************************/
+pauseBtn.addEventListener('click', ()=> {
+    pauseResume();
+})
+pauseBtn.addEventListener('touchstart', ()=> {
+    pauseResume();
 })
 /*************************************/
-function start(){
-    
+function start() {
+
     addClass(startBtn, 'unvisible');
     setTimeout(() => {
         addClass(pauseBtn, 'visible');
         addClass(lapBtn, 'visible');
     }, 500);
-
+    
     startTime = new Date().getTime();
-    mainInterval = setInterval(() => {
-        updateWatch(startTime, mainMinSecSpan, mainMillSpan);
-    }, 10);
+    setStopWatchInterval();
+}
+/*************************************/
+function pauseResume() {
+    if (pauseBtn.textContent === 'Pause') {
+
+        pauseBtn.textContent = 'Resume';
+        stopTime = new Date().getTime();
+        clearInterval(mainInterval);
+        return;
+    }
+    pauseBtn.textContent = 'Pause';
+    pausedTime = new Date().getTime() - stopTime;
+    startTime += pausedTime;
+    setStopWatchInterval(); 
+
+
 }
 /*************************************/
 //calculating the time that has passed since clicking or touching start btn
-function updateWatch(startingTime, minSecSpan, millSpan){
+function updateWatch(startingTime, minSecSpan, millSpan) {
     let currentTime = new Date().getTime();
     let diffrence = currentTime - startingTime;
-    
+
     let millSecs = Math.floor(diffrence / 10);
     let seconds = Math.floor(diffrence / 1000);
     let minutes = Math.floor(diffrence / 1000 / 60);
-    
+
     let displayMillSecs = millSecs - (seconds * 100);
     let displaySecs = seconds - (minutes * 60);
     let displayMins = (minutes < 10) ? `0${minutes}` : minutes;
-    
+
     displayMillSecs = (displayMillSecs < 10) ? `0${displayMillSecs}` : displayMillSecs;
     displaySecs = (displaySecs < 10) ? `0${displaySecs}` : displaySecs;
 
     minSecSpan.textContent = `${displayMins}:${displaySecs}`;
-    millSpan.textContent = `:${displayMillSecs}`;  
+    millSpan.textContent = `:${displayMillSecs}`;
 }
 /*************************************/
 function addClass(element, targetclass) {
     element.classList.add(targetclass);
+}
+/*************************************/
+function setStopWatchInterval(intervalName){
+    if(intervalName === 'lap'){
+        return;
+    }
+    mainInterval = setInterval(() => {
+        updateWatch(startTime, mainMinSecSpan, mainMillSpan);
+    }, 10);
 }
 
