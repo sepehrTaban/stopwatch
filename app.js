9897410bc79a93f8654c8815f5a3af967e52a4ac
Sepehr Taban <sepehr.taban77@gmail.com>
2021-07-10 22:02:55 +0430

    refrencing DOM nodes && declaring variables

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-f
+/*#################### refrencing DOM nodes && declaring variables ###################*/
+/*#################### refrencing DOM nodes && declaring variables ###################*/
+/*#################### refrencing DOM nodes && declaring variables ###################*/
+const stopWatchContainer = document.querySelector('.watch-container');
+const mainMinuteSeconds = document.querySelector('.min-sec');
+const mainMill = document.querySelector('.mill');
+const lapMinuteSeconds = document.querySelector('.lap-min-sec');
+const lapMill = document.querySelector('.lap-mill');
+const lapTable = document.querySelector('.laps-table');
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause-resume');
+const lapBtn = document.querySelector('.lap-reset');
+
+let mainInterval;
+let startTime;
+let stopTime;
+let pausedTime;
+
+let lapInterval = false;
+let lapStart;
+let lapStop;
+let lapPaused;
+let lapCount = 0;
+
